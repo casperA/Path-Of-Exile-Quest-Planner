9020b3b1d7f8a86032f2aebaa3ebd7d73563f21a
casperA <lucas@gayagenda.gop>
2014-12-26 14:55:57 -0500

    Test push

--- PathOfExileQuestPlanner/src/QuestReward.java
+++ PathOfExileQuestPlanner/src/QuestReward.java
@@ -1,7 +1,10 @@
+import java.util.Vector;
+
 
 public class QuestReward {
 
 	//A choice of gems
+	private Vector<SkillGem>[] rewardGemOptions ;
 	//A choice of items
 	//Books (items)
 	//Dealing with the bandits
